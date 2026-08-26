00ab776523cfd96f7c68c2c5bc71a97eb5a8ec82
mdkadirkhan40391-jpg <mdkadirkhan40391@gmail.com>
2026-08-26 19:05:01 +0600

    Delete app/src/main/java/com/automation/encryptiontool/domain directory

--- app/src/main/java/com/automation/encryptiontool/domain/repository/SettingsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.automation.encryptiontool.domain.repository
-
-import com.automation.encryptiontool.domain.model.SettingsModel
-import kotlinx.coroutines.flow.Flow
-
-interface SettingsRepository {
-    fun observeSettings(): Flow<SettingsModel>
-    suspend fun updateSettings(settings: SettingsModel)
-}
--- app/src/main/java/com/automation/encryptiontool/domain/security/Encryptor.kt
+++
@@ -1,8 +0,0 @@
-package com.automation.encryptiontool.domain.security
-
-import com.automation.encryptiontool.domain.model.EncryptedPayload
-
-interface Encryptor {
-    fun encrypt(plainText: String, securityCode: String, keyVersion: Int = 1): EncryptedPayload
-    fun decrypt(payload: EncryptedPayload, securityCode: String): String
-}
